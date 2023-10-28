283b7db69ad950dde3e1af8a229ad7ba2aec0181
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-28 22:22:31 +0200

    Adjust verbose

--- Public/Add-InfobloxFixedAddress.ps1
+++ Public/Add-InfobloxFixedAddress.ps1
@@ -28,7 +28,7 @@
     )
 
     if (-not $Script:InfobloxConfiguration) {
-        Write-Warning -Message 'Get-InfobloxNetwork - You must first connect to an Infoblox server using Connect-Infoblox'
+        Write-Warning -Message 'Add-InfobloxFixedAddress - You must first connect to an Infoblox server using Connect-Infoblox'
         return
     }
 
@@ -42,10 +42,10 @@
             mac      = $MacAddress.ToLower()
         }
     }
-    $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+    $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat -WarningAction SilentlyContinue -WarningVariable varWarning
     if ($Output) {
         Write-Verbose -Message "Add-InfobloxFixedAddress - Removed $($Mac.ipv4addr) with mac address $($Mac.mac) / $Output"
     } else {
-        Write-Warning -Message "Add-InfobloxFixedAddress - Failed to remove $($Mac.ipv4addr) with mac address $($Mac.mac) / $Output"
+        Write-Warning -Message "Add-InfobloxFixedAddress - Failed to remove $($Mac.ipv4addr) with mac address $($Mac.mac), error: $varWarning"
     }
 }
