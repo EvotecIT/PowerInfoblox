bccd6c431b598b7edb7f88fdb9786072a4fcf5f0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-28 09:40:33 +0200

    Schema method

---
+++ Public/Get-InfobloxSchema.ps1
@@ -0,0 +1,20 @@
+﻿function Get-InfobloxSchema {
+    [CmdletBinding()]
+    param()
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxSchema - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    $Query = Invoke-InfobloxQuery -RelativeUri "?_schema" -Method Get
+    if ($Query) {
+        [PSCustomObject] @{
+            RequestedVersion  = $Query.requested_version
+            SupportedObjects  = $Query.supported_objects
+            SupportedVersions = $Query.supported_versions
+        }
+    } else {
+        Write-Warning -Message 'Get-InfobloxSchema - No schema returned'
+    }
+}
+
