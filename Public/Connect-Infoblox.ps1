3f5cacde47cc52a4c54ac9ffc91e137791079fcc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-28 09:40:16 +0200

    Add connection method

---
+++ Public/Connect-Infoblox.ps1
@@ -0,0 +1,25 @@
+﻿function Connect-Infoblox {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $Server,
+        [Parameter(Mandatory)][pscredential] $Credential,
+        [Parameter()][string] $ApiVersion = '1.0',
+        [switch] $AllowSelfSignedCerts
+    )
+
+    if ($AllowSelfSignedCerts) {
+        Hide-SelfSignedCerts
+    }
+
+    $PSDefaultParameterValues['Invoke-InfobloxQuery:Credential'] = $Credential
+    $PSDefaultParameterValues['Invoke-InfobloxQuery:Server'] = $Server
+    #$PSDefaultParameterValues['Invoke-InfobloxQuery:ApiVersion'] = $ApiVersion
+    $PSDefaultParameterValues['Invoke-InfobloxQuery:BaseUri'] = "https://$Server/wapi/v$apiVersion"
+
+    $Script:InfobloxConfiguration = [ordered] @{
+        ApiVersion = $ApiVersion
+        Credential = $Credential
+        Server     = $Server
+        BaseUri    = "https://$Server/wapi/v$apiVersion"
+    }
+}
