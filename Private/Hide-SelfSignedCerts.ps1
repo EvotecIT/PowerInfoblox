533ead9ba9288d5128c105880523423f5b4d69de
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-28 09:39:49 +0200

    Hide certificates

---
+++ Private/Hide-SelfSignedCerts.ps1
@@ -0,0 +1,24 @@
+﻿function Hide-SelfSignedCerts {
+    [cmdletbinding()]
+    param(
+
+    )
+    
+    try {
+        Add-Type -TypeDefinition @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy {
+            public bool CheckValidationResult(
+                ServicePoint srvPoint, X509Certificate certificate,
+                WebRequest request, int certificateProblem) {
+                return true;
+            }
+        }
+"@
+
+        [System.Net.ServicePointManager]::CertificatePolicy = [TrustAllCertsPolicy]::new()
+    } catch {
+        Write-Warning -Message "Hide-SelfSignedCerts - Error when trying to hide self-signed certificates: $($_.Exception.Message)"
+    }
+}
