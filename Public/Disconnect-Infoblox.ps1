5724d17d7cced99f34aa99babc39c24ac4a3c902
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-28 09:40:23 +0200

    Clear credentials

---
+++ Public/Disconnect-Infoblox.ps1
@@ -0,0 +1,19 @@
+﻿function Disconnect-Infoblox {
+    <#
+    .SYNOPSIS
+    Disconnects from an InfoBlox server
+
+    .DESCRIPTION
+    Disconnects from an InfoBlox server
+    As this is a REST API it doesn't really disconnect, but it does clear the script variable to clear the credentials from memory
+
+    .EXAMPLE
+    Disconnect-Infoblox
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding()]
+    param()
+    $Script:InfobloxConfiguration = $null
+}
