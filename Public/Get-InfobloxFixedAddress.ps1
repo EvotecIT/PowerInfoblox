c03dc3c0ef19886e850810861c59bb741dc52601
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-28 19:48:14 +0200

    Add Get-InfobloxFixedAddress function to retrieve fixed addresses from Infoblox server.
    - Creates a new PowerShell function called Get-InfobloxFixedAddress to retrieve fixed addresses from an Infoblox server.
    - The function takes a mandatory parameter called MacAddress and an optional switch parameter called PartialMatch.
    - If the Infoblox server is not connected, a warning is displayed.
    - The function makes a GET request to the 'fixedaddress' relative URI to retrieve the fields 'mac', 'ipv4addr', and 'network_view'.
    - If the PartialMatch switch is specified, the MacAddress is used for partial matching.
    - Otherwise, the MacAddress is used for exact matching.

---
+++ Public/Get-InfobloxFixedAddress.ps1
@@ -0,0 +1,28 @@
+﻿function Get-InfobloxFixedAddress {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory)][string] $MacAddress,
+        [switch] $PartialMatch
+    )
+
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxNetwork - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    Write-Verbose -Message "Get-InfobloxFixedAddress - Requesting MacAddress [$MacAddress] / PartialMatch [$($PartialMatch.IsPresent)]"
+
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'fixedaddress'
+        Method         = 'GET'
+        QueryParameter = @{
+            _return_fields = 'mac,ipv4addr,network_view'
+        }
+    }
+    if ($PartialMatch) {
+        $invokeInfobloxQuerySplat.QueryParameter."mac~" = $MacAddress.ToLower()
+    } else {
+        $invokeInfobloxQuerySplat.QueryParameter.mac = $MacAddress.ToLower()
+    }
+    Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+}
