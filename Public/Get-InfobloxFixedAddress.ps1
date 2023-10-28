0e7273e9f5fd8d8a967a6e1dd6204ff9289c722c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-28 22:22:38 +0200

    adjust warning

--- Public/Get-InfobloxFixedAddress.ps1
+++ Public/Get-InfobloxFixedAddress.ps1
@@ -6,7 +6,7 @@
     )
 
     if (-not $Script:InfobloxConfiguration) {
-        Write-Warning -Message 'Get-InfobloxNetwork - You must first connect to an Infoblox server using Connect-Infoblox'
+        Write-Warning -Message 'Get-InfobloxFixedAddress - You must first connect to an Infoblox server using Connect-Infoblox'
         return
     }
 
