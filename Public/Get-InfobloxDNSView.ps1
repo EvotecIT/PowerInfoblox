057e405346f724693400d66c1a6b3024c197e6ad
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-29 08:12:52 +0100

    Add new command

---
+++ Public/Get-InfobloxDNSView.ps1
@@ -0,0 +1,22 @@
+﻿function Get-InfobloxDNSView {
+    [cmdletbinding()]
+    param(
+
+    )
+
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxDNSView - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    Write-Verbose -Message "Get-InfobloxDNSView - Requesting DNS View"
+
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'view'
+        Method         = 'GET'
+        QueryParameter = @{
+            # _return_fields = 'mac,ipv4addr,network_view'
+        }
+    }
+    Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+}
