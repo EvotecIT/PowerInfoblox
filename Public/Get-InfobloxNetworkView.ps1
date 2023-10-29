cafd9835c3f02f9ce648edcb604baf62fae43044
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-29 14:46:33 +0100

    Add Get-InfobloxNetworkView function to retrieve network view from Infoblox server

---
+++ Public/Get-InfobloxNetworkView.ps1
@@ -0,0 +1,22 @@
+﻿function Get-InfobloxNetworkView {
+    [cmdletbinding()]
+    param(
+
+    )
+
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxNetworkView - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    Write-Verbose -Message "Get-InfobloxNetworkView - Requesting Network View"
+
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'networkview'
+        Method         = 'GET'
+        QueryParameter = @{
+            # _return_fields = 'mac,ipv4addr,network_view'
+        }
+    }
+    Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+}
