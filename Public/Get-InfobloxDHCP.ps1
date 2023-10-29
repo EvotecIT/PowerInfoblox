3f54b27be27dbc851bf5269af5d3def1c1f63c74
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-29 08:09:23 +0100

    Get DHCP settings and leases

---
+++ Public/Get-InfobloxDHCP.ps1
@@ -0,0 +1,31 @@
+﻿function Get-InfobloxDHCP {
+    [CmdletBinding()]
+    param(
+        [string] $Network,
+        [switch] $PartialMatch
+    )
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxDHCP - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+    if ($Network) {
+        Write-Verbose -Message "Get-InfobloxDHCP - Requesting DHCP ranges for Network [$Network] / PartialMatch [$($PartialMatch.IsPresent)]"
+    } else {
+        Write-Verbose -Message "Get-InfobloxDHCP - Requesting DHCP ranges for all networks"
+    }
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'range'
+        Method         = 'GET'
+        QueryParameter = @{
+            # _return_fields = 'mac,ipv4addr,network_view'
+        }
+    }
+    if ($Network) {
+        if ($PartialMatch) {
+            $invokeInfobloxQuerySplat.QueryParameter."network~" = $Network.ToLower()
+        } else {
+            $invokeInfobloxQuerySplat.QueryParameter.network = $Network.ToLower()
+        }
+    }
+    Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+}
