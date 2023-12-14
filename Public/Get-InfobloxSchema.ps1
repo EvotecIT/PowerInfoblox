74eb39ae8b94fc28f01c38df2775267678ab4090
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-14 09:12:48 +0100

    Allow querying schema for objects

--- Public/Get-InfobloxSchema.ps1
+++ Public/Get-InfobloxSchema.ps1
@@ -1,20 +1,48 @@
 ﻿function Get-InfobloxSchema {
+    <#
+    .SYNOPSIS
+    Get the schema for Infoblox as a whole or a specific object
+
+    .DESCRIPTION
+    Get the schema for Infoblox as a whole or a specific object
+
+    .PARAMETER Object
+    The object to get the schema for
+
+    .EXAMPLE
+    Get-InfobloxSchema
+
+    .EXAMPLE
+    Get-InfobloxSchema -Object 'record:host'
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param(
-
+        [string] $Object
     )
     if (-not $Script:InfobloxConfiguration) {
         Write-Warning -Message 'Get-InfobloxSchema - You must first connect to an Infoblox server using Connect-Infoblox'
         return
     }
-
-    $Query = Invoke-InfobloxQuery -RelativeUri "?_schema" -Method Get
-    if ($Query) {
-        [PSCustomObject] @{
-            RequestedVersion  = $Query.requested_version
-            SupportedObjects  = $Query.supported_objects
-            SupportedVersions = $Query.supported_versions
+    if ($Object) {
+        $invokeInfobloxQuerySplat = @{
+            RelativeUri = "$($Object.ToLower())"
+            Method      = 'Get'
+            Query       = @{
+                _schema = $true
+            }
         }
+    } else {
+        $invokeInfobloxQuerySplat = @{
+            RelativeUri = "?_schema"
+            Method      = 'Get'
+        }
+    }
+    $Query = Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+    if ($Query) {
+        $Query
     } else {
         Write-Warning -Message 'Get-InfobloxSchema - No schema returned'
     }
