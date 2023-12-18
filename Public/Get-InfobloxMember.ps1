28e06bc6cccd533c2ee5cae6039e425c33e33896
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-18 19:11:52 +0100

    Add Get-InfobloxMember function to retrieve Infoblox member details

---
+++ Public/Get-InfobloxMember.ps1
@@ -0,0 +1,21 @@
+﻿function Get-InfobloxMember {
+    [cmdletbinding()]
+    param(
+
+    )
+
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxMember - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'member'
+        Method         = 'GET'
+        QueryParameter = @{
+            _return_fields = 'config_addr_type,host_name,platform,service_type_configuration,vip_setting,node_info,service_status'
+        }
+    }
+    $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+    $Output | Select-ObjectByProperty -LastProperty '_ref'
+}
