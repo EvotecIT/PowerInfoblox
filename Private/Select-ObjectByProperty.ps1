4c17e96c54cffe3c7dc2e0ccfd1620d0fc507820
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-18 18:47:17 +0100

    Add Select-ObjectByProperty function to handle object properties

---
+++ Private/Select-ObjectByProperty.ps1
@@ -0,0 +1,29 @@
+﻿function Select-ObjectByProperty {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)][Array] $Object,
+        [alias('FirstProperty')][Parameter()][string[]] $FirstProperties,
+        [alias('LastProperty')][Parameter()][string[]] $LastProperties
+    )
+    process {
+        foreach ($O in $Object) {
+            $ReturnObject = [ordered] @{}
+            foreach ($Property in $FirstProperties) {
+                if ($O.PSObject.Properties.Name -contains $Property) {
+                    $ReturnObject[$Property] = $O.$Property
+                }
+            }
+            foreach ($Property in $O.PSObject.Properties.Name) {
+                if ($Property -notin $LastProperties -and $Property -notin $FirstProperties) {
+                    $ReturnObject[$Property] = $O.$Property
+                }
+            }
+            foreach ($Property in $LastProperties) {
+                if ($O.PSObject.Properties.Name -contains $Property) {
+                    $ReturnObject[$Property] = $O.$Property
+                }
+            }
+            [pscustomobject]$ReturnObject
+        }
+    }
+}
