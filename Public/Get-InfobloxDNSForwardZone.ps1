e742fca8633c7c60162780c23731282f2c4a84b2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-18 14:03:12 +0100

    Add Get-InfobloxDNSForwardZone function to retrieve DNS forward zones

---
+++ Public/Get-InfobloxDNSForwardZone.ps1
@@ -0,0 +1,27 @@
+﻿function Get-InfobloxDNSForwardZone {
+    [cmdletbinding()]
+    param(
+        [string] $Name,
+        [string] $View
+    )
+
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxDNSForwardZone - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'zone_forward'
+        Method         = 'GET'
+        QueryParameter = @{
+            _max_results = 1000000
+        }
+    }
+    if ($View) {
+        $invokeInfobloxQuerySplat.QueryParameter.view = $View.ToLower()
+    }
+    if ($Name) {
+        $invokeInfobloxQuerySplat.QueryParameter.fqdn = $Name.ToLower()
+    }
+    Invoke-InfobloxQuery @invokeInfobloxQuerySplat | Select-ObjectByProperty -LastProperty '_ref'
+}
