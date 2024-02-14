98044ef48fbfcfcb3bf4ab27c2cb930fbfca7675
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-14 21:33:48 +0100

    Add Get-InfobloxPermission function to retrieve Infoblox permissions

---
+++ Public/Get-InfobloxPermission.ps1
@@ -0,0 +1,27 @@
+﻿function Get-InfobloxPermission {
+    [cmdletbinding()]
+    param(
+        [switch] $FetchFromSchema
+    )
+
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxPermissions - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    # defalt return fields
+    if ($FetchFromSchema) {
+        $ReturnFields = Get-FieldsFromSchema -SchemaObject "permission"
+    }
+
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'permission'
+        Method         = 'GET'
+        QueryParameter = @{
+            _max_results = 1000000
+            _return_fields = $ReturnFields
+        }
+    }
+    $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+    $Output | Select-ObjectByProperty -LastProperty '_ref'
+}
