72b9c604beecef9cb8eb8397a9846f07c99f338c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-14 21:32:59 +0100

    Add Get-InfobloxDHCPRange function

---
+++ Public/Get-InfobloxDHCPRange.ps1
@@ -0,0 +1,39 @@
+﻿function Get-InfobloxDHCPRange {
+    [CmdletBinding()]
+    param(
+        [string] $Network,
+        [switch] $PartialMatch,
+        [switch] $FetchFromSchema
+    )
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxDHCPRange - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+    if ($Network) {
+        Write-Verbose -Message "Get-InfobloxDHCPRange - Requesting DHCP ranges for Network [$Network] / PartialMatch [$($PartialMatch.IsPresent)]"
+    } else {
+        Write-Verbose -Message "Get-InfobloxDHCPRange - Requesting DHCP ranges for all networks"
+    }
+
+    if ($FetchFromSchema) {
+        $ReturnFields = Get-FieldsFromSchema -SchemaObject "range"
+    } else {
+        $ReturnFields = $Null
+    }
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'range'
+        Method         = 'GET'
+        QueryParameter = @{
+            _max_results   = 1000000
+            _return_fields = $ReturnFields
+        }
+    }
+    if ($Network) {
+        if ($PartialMatch) {
+            $invokeInfobloxQuerySplat.QueryParameter."network~" = $Network.ToLower()
+        } else {
+            $invokeInfobloxQuerySplat.QueryParameter.network = $Network.ToLower()
+        }
+    }
+    Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+}
