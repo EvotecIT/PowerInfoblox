a49076ab53bcd86d3cb01aef6aa558269db64d83
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-14 21:33:58 +0100

    Add Get-InfobloxVDiscoveryTask function to retrieve vdiscoverytask from Infoblox server

---
+++ Public/Get-InfobloxVDiscoveryTask.ps1
@@ -0,0 +1,27 @@
+﻿function Get-InfobloxVDiscoveryTask {
+    [cmdletbinding()]
+    param(
+        [switch] $FetchFromSchema
+    )
+
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxVDiscoveryTask - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    # defalt return fields
+    if ($FetchFromSchema) {
+        $ReturnFields = Get-FieldsFromSchema -SchemaObject "vdiscoverytask"
+    }
+
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'vdiscoverytask'
+        Method         = 'GET'
+        QueryParameter = @{
+            _max_results = 1000000
+            _return_fields = $ReturnFields
+        }
+    }
+    $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+    $Output | Select-ObjectByProperty -LastProperty '_ref'
+}
