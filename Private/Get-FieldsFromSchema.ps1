99c2c37c76c31f46b5e65be7b08b880ca7ced67a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-14 20:15:13 +0100

    Add Get-FieldsFromSchema function to retrieve fields from a schema

---
+++ Private/Get-FieldsFromSchema.ps1
@@ -0,0 +1,28 @@
+﻿function Get-FieldsFromSchema {
+    [CmdletBinding()]
+    param(
+        [PSCustomobject] $Schema,
+        [string] $SchemaObject
+    )
+    if (-not $Script:InfobloxSchemaFields) {
+        $Script:InfobloxSchemaFields = [ordered] @{}
+    }
+    if ($Script:InfobloxSchemaFields[$SchemaObject]) {
+        $Script:InfobloxSchemaFields[$SchemaObject]
+    } else {
+        if (-not $Schema) {
+            $Schema = Get-InfobloxSchema -Object $SchemaObject
+        }
+        if ($Schema -and $Schema.fields.name) {
+            $FilteredFields = foreach ($Field in $Schema.fields) {
+                if ($Field.supports -like "*r*") {
+                    $Field.Name
+                }
+            }
+            $Script:InfobloxSchemaFields[$SchemaObject] = ($FilteredFields -join ',')
+            $Script:InfobloxSchemaFields[$SchemaObject]
+        } else {
+            Write-Warning -Message "Get-FieldsFromSchema - Failed to fetch schema for record type 'allrecords'. Using defaults"
+        }
+    }
+}
