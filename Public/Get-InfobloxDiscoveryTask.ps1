75eb4ff4e66970300b29b28ee28b1b8168372155
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-14 21:33:03 +0100

    Add Get-InfobloxDiscoveryTask function to fetch discovery tasks from Infoblox server

---
+++ Public/Get-InfobloxDiscoveryTask.ps1
@@ -0,0 +1,27 @@
+﻿function Get-InfobloxDiscoveryTask {
+    [cmdletbinding()]
+    param(
+        [switch] $FetchFromSchema
+    )
+
+    if (-not $Script:InfobloxConfiguration) {
+        Write-Warning -Message 'Get-InfobloxDiscoveryTask - You must first connect to an Infoblox server using Connect-Infoblox'
+        return
+    }
+
+    # defalt return fields
+    if ($FetchFromSchema) {
+        $ReturnFields = Get-FieldsFromSchema -SchemaObject "discoverytask"
+    }
+
+    $invokeInfobloxQuerySplat = @{
+        RelativeUri    = 'discoverytask'
+        Method         = 'GET'
+        QueryParameter = @{
+            _max_results = 1000000
+            _return_fields = $ReturnFields
+        }
+    }
+    $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat
+    $Output | Select-ObjectByProperty -LastProperty '_ref'
+}
