00e6d84a1a89048a5ba3bdb8b25ba6d733a20c43
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-15 11:25:30 +0100

    Refactor Select-ObjectByProperty to allow user-defined properties

--- Private/Select-ObjectByProperty.ps1
+++ Private/Select-ObjectByProperty.ps1
@@ -4,13 +4,18 @@
         [Parameter(Position = 0, ValueFromPipeline)][Array] $Object,
         [System.Collections.IDictionary] $AddObject,
         [alias('FirstProperty')][Parameter()][string[]] $FirstProperties,
-        [alias('LastProperty')][Parameter()][string[]] $LastProperties
+        [alias('LastProperty')][Parameter()][string[]] $LastProperties,
+        [string[]] $AllProperties
     )
     process {
         foreach ($O in $Object) {
+            # If we have an object, we can get the properties from it
+            # we assume user can provide AllProperties instead of current object properties
+            $Properties = if ($AllProperties) { $AllProperties } else { $O.PSObject.Properties.Name }
+
             $ReturnObject = [ordered] @{}
             foreach ($Property in $FirstProperties) {
-                if ($O.PSObject.Properties.Name -contains $Property) {
+                if ($Properties -contains $Property) {
                     $ReturnObject[$Property] = $O.$Property
                 }
             }
@@ -19,13 +24,13 @@
                     $ReturnObject[$Property] = $AddObject[$Property]
                 }
             }
-            foreach ($Property in $O.PSObject.Properties.Name) {
+            foreach ($Property in $Properties) {
                 if ($Property -notin $LastProperties -and $Property -notin $FirstProperties) {
                     $ReturnObject[$Property] = $O.$Property
                 }
             }
             foreach ($Property in $LastProperties) {
-                if ($O.PSObject.Properties.Name -contains $Property) {
+                if ($Properties -contains $Property) {
                     $ReturnObject[$Property] = $O.$Property
                 }
             }
