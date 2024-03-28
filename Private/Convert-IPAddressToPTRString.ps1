3c8ed7f437c97444e77553ed3c93cade5cd58658
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-28 09:12:23 +0100

    Add convert ip to PTR

---
+++ Private/Convert-IPAddressToPTRString.ps1
@@ -0,0 +1,18 @@
+﻿function Convert-IpAddressToPtrString {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$IPAddress
+    )
+
+    # Split the IP address into its octets
+    $octets = $IPAddress -split "\."
+
+    # Reverse the octets
+    [array]::Reverse($octets)
+
+    # Join the reversed octets with dots and append the standard PTR suffix
+    $ptrString = ($octets -join ".") + ".in-addr.arpa"
+
+    $ptrString
+}
