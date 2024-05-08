48156aeea99b19c4bc2286e54f6ec300ec93f9d2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-08 11:28:19 +0200

    Add docs

--- Public/Add-InfobloxSubnet.ps1
+++ Public/Add-InfobloxSubnet.ps1
@@ -1,4 +1,41 @@
 ﻿function Add-InfobloxSubnet {
+    <#
+    .SYNOPSIS
+    Adds a subnet to Infoblox.
+
+    .DESCRIPTION
+    This function adds a subnet to Infoblox. It requires a connection to an Infoblox server, which can be established using the Connect-Infoblox function.
+
+    .PARAMETER Subnet
+    The subnet to add. This parameter is mandatory.
+
+    .PARAMETER Comment
+    An optional comment for the subnet.
+
+    .PARAMETER NetworkView
+    The network view in which to add the subnet. Defaults to 'default'.
+
+    .PARAMETER AutoCreateReverseZone
+    A switch that, when present, indicates that a reverse zone should be automatically created for the subnet.
+
+    .PARAMETER DHCPGateway
+    The DHCP gateway for the subnet.
+
+    .PARAMETER DHCPLeaseTime
+    The DHCP lease time for the subnet.
+
+    .PARAMETER DHCPDomainNameServers
+    The DHCP domain name servers for the subnet.
+
+    .PARAMETER ReturnOutput
+    A switch that, when present, indicates that the output of the command should be returned.
+
+    .EXAMPLE
+    Add-InfobloxSubnet -Subnet '192.168.1.0/24' -Comment 'Test subnet' -DHCPGateway '192.168.1.1'
+
+    .NOTES
+    This function requires a connection to an Infoblox server, which can be established using the Connect-Infoblox function.
+    #>
     [CmdletBinding(SupportsShouldProcess)]
     param(
         [Parameter(Mandatory)][string] $Subnet,
@@ -7,7 +44,8 @@
         [switch] $AutoCreateReverseZone,
         [string] $DHCPGateway,
         [string] $DHCPLeaseTime,
-        [string] $DHCPDomainNameServers
+        [string] $DHCPDomainNameServers,
+        [switch] $ReturnOutput
     )
 
     if (-not $Script:InfobloxConfiguration) {
@@ -77,10 +115,9 @@
 
     $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat
     if ($Output) {
-        if (-not $Suppress) {
+        Write-Verbose -Message "Add-InfobloxSubnet - $Output"
+        if ($ReturnOutput) {
             $Output
-        } else {
-            Write-Verbose -Message "Add-InfobloxSubnet - $Output"
         }
     }
 }
