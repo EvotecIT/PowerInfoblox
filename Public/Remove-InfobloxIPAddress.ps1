400fad3c3bc3eca57a47d6af0f29fc1ed8467938
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-10 13:14:41 +0200

    Remove dead code and add docs

--- Public/Remove-InfobloxIPAddress.ps1
+++ Public/Remove-InfobloxIPAddress.ps1
@@ -1,4 +1,21 @@
 ﻿function Remove-InfobloxIPAddress {
+    <#
+    .SYNOPSIS
+    Removes an IP address from Infoblox.
+
+    .DESCRIPTION
+    This function removes an IP address from Infoblox. It checks for an existing connection to an Infoblox server, established via Connect-Infoblox, before attempting the removal. The function supports verbose output for detailed operation insights.
+
+    .PARAMETER IPv4Address
+    The IPv4 address to be removed from Infoblox. This parameter is mandatory.
+
+    .EXAMPLE
+    Remove-InfobloxIPAddress -IPv4Address '192.168.1.100'
+    Removes the IP address 192.168.1.100 from Infoblox.
+
+    .NOTES
+    Ensure you are connected to an Infoblox server using Connect-Infoblox before executing this function. The function uses Write-Verbose for detailed operation output, which can be enabled by setting $VerbosePreference or using the -Verbose switch.
+    #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
         [parameter()][string] $IPv4Address
@@ -20,14 +37,9 @@
             RelativeUri = "$($IP._ref)"
             Method      = 'DELETE'
         }
-        $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat #-WarningAction SilentlyContinue -WarningVariable varWarning
+        $Output = Invoke-InfobloxQuery @invokeInfobloxQuerySplat
         if ($Output) {
             Write-Verbose -Message "Remove-InfobloxIPAddress - Removed $($IP.ip_address) from network $($IP.network) / $Output"
         }
-        #else {
-        # if (-not $WhatIfPreference) {
-        #      Write-Warning -Message "Remove-InfobloxIPAddress - Failed to remove $($IP.ip_address) with mac address $($IP.network), error: $varWarning"
-        # }
-        #}
     }
 }
