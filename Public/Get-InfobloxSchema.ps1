41123f824e4fa261d08500776d31a5898cb928d2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-21 21:55:03 +0200

    chore: Refactor Get-InfobloxSchema.ps1 to improve parameter handling

--- Public/Get-InfobloxSchema.ps1
+++ Public/Get-InfobloxSchema.ps1
@@ -9,6 +9,15 @@
     .PARAMETER Object
     The object to get the schema for
 
+    .PARAMETER ReturnReadOnlyFields
+    Return only read-only fields in format suitable for use with Invoke-InfobloxQuery
+
+    .PARAMETER ReturnWriteFields
+    Return only write fields in format suitable for use with Invoke-InfobloxQuery
+
+    .PARAMETER ReturnFields
+    Return all fields in full objects
+
     .EXAMPLE
     Get-InfobloxSchema
 
@@ -21,7 +30,9 @@
     [CmdletBinding()]
     param(
         [string] $Object,
-        [switch] $ReturnReadOnlyFields
+        [switch] $ReturnReadOnlyFields,
+        [switch] $ReturnWriteFields,
+        [switch] $ReturnFields
     )
     if (-not $Script:InfobloxConfiguration) {
         if ($ErrorActionPreference -eq 'Stop') {
@@ -48,6 +59,11 @@
     if ($Query) {
         if ($ReturnReadOnlyFields) {
             Get-FieldsFromSchema -Schema $Query -SchemaObject $Object
+        } elseif ($ReturnWriteFields) {
+            $Fields = ((Get-InfobloxSchema -Object $Object).Fields | Where-Object { $_.supports -like "*r*" }).Name
+            $Fields -join ','
+        } elseif ($ReturnFields) {
+            (Get-InfobloxSchema -Object $Object).Fields
         } else {
             $Query
         }
