9bc6d6e9a74beb87570f451e13734f03c510dc33
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-29 09:08:03 +0200

    feat: Add New-InfobloxOption function for creating dummy Infoblox options

---
+++ Public/New-InfobloxOption.ps1
@@ -0,0 +1,58 @@
+﻿function New-InfobloxOption {
+    <#
+    .SYNOPSIS
+    Creates a dummy Infoblox option to use within other cmdlets
+
+    .DESCRIPTION
+    This function creates a new Infoblox option. It's just syntactic sugar to make it easier to create options to use within other cmdlets.
+
+    .PARAMETER Name
+    The name of the Infoblox option. This parameter is mandatory.
+
+    .PARAMETER Number
+    The number of the Infoblox option. This parameter is mandatory.
+
+    .PARAMETER UseOption
+    A switch indicating whether to use the option. This parameter is mandatory.
+
+    .PARAMETER Value
+    The value of the Infoblox option. This parameter is mandatory.
+
+    .PARAMETER VendorClass
+    The vendor class of the Infoblox option. This parameter is mandatory.
+
+    .EXAMPLE
+    $addInfobloxDHCPRangeSplat = @{
+        StartAddress = '10.10.12.5'
+        EndAddress   = '10.10.12.10'
+        Options      = @(
+            New-InfobloxOption -Name "dhcp-lease-time" -Number 51 -UseOption -Value '86400' -VendorClass 'DHCP'
+            New-InfobloxOption -Name "domain-name-servers" -Number 6 -UseOption -Value '192.168.0.15' -VendorClass 'DHCP'
+            New-InfobloxOption -Name 'routers' -Number 3 -UseOption -Value '192.168.11.12' -VendorClass 'DHCP'
+            New-InfobloxOption -Name 'time-servers' -Number 4 -UseOption -Value '11' -VendorClass 'DHCP'
+        )
+        Verbose      = $true
+    }
+
+    Add-InfobloxDHCPRange @addInfobloxDHCPRangeSplat
+
+    .NOTES
+    This function is used to create a dummy Infoblox option to use within other cmdlets.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $Name,
+        [Parameter(Mandatory)][alias('Num')][int] $Number,
+        [Parameter(Mandatory)][switch] $UseOption,
+        [Parameter(Mandatory)][string] $Value,
+        [Parameter(Mandatory)][string] $VendorClass
+    )
+
+    [ordered] @{
+        "name"         = $Name
+        "num"          = $Number
+        "use_option"   = $UseOption.IsPresent
+        "value"        = $Value
+        "vendor_class" = $VendorClass
+    }
+}
