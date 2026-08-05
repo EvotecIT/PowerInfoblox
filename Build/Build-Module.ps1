65c8067608c31d8417b77c028c454127a93feb00
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-08-05 17:50:22 +0200

    update

--- Build/Build-Module.ps1
+++ Build/Build-Module.ps1
@@ -114,8 +114,8 @@ Build-Module -ModuleName 'PowerInfoblox' {
     } -CopyFilesRelative -IncludeTagName
 
     # global options for publishing to github/psgallery
-    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
-    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
+    New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
+    New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
 
     New-ConfigurationGate -Mode $ConfigurationGateMode
 } -ExitCode
